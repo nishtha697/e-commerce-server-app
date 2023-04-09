76b6285c330e9cd42ee0aac4128526d37ca0a8ad
Nishtha Goswami <nishthagoswami@Nishthas-MacBook-Pro.local>
2023-04-09 02:40:17 -0400

    Buyer schema validation

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ import SellerController from "./controllers/sellers-controller.js"
 import BuyerController from "./controllers/buyers-controller.js"
 import ProductController from "./controllers/products-controller.js";
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
 import cors from 'cors';
 
 mongoose.connect('mongodb+srv://GoswamiNGuptaS:Mp0Sp7qX0gyfcmtC@cs5200project.uko8lgd.mongodb.net/e-commerce?retryWrites=true&w=majority');
 
-
 const app = express()
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
 
 ProductController(app)
 SellerController(app)
--- buyers/buyer-schema.js
+++ buyers/buyer-schema.js
@@ -1,12 +1,41 @@
 import mongoose from 'mongoose';
+
 const schema = mongoose.Schema({
-                                   username: { type: String, required: [true, "Buyer username required"] },
-                                   password: { type: String, required: [true, "Buyer password required"] },
-                                   first_name: { type: String, required: [true, "Buyer first name required"] },
-                                   last_name: { type: String, required: [true, "Buyer last name required"] },
-                                   email: { type: String, required: [true, "Buyer email required"] },
-                                   addresses: { type: Array, required: [true, "Buyer address required"] },
-                                   phone: { type: Number, required: [true, "Buyer phone number required"] },
-                                   dob: { type: Number, required: [true, "Buyer dob required"] },
+                                   _id: {type: Number, required: [true, "Buyer id required"]},
+                                   username: {
+                                       type: String,
+                                       required: [true, "Buyer username required"]
+                                   },
+                                   password: {
+                                       type: String,
+                                       required: [true, "Buyer password required"],
+                                       minlength:[6,'Minimum password length is 6 characters']
+                                   },
+                                   first_name: {
+                                       type: String,
+                                       required: [true, "Buyer first name required"]
+                                   },
+                                   last_name: {
+                                       type: String,
+                                       required: [true, "Buyer last name required"]
+                                   },
+                                   email: {type: String, required: [true, "Buyer email required"]},
+                                   addresses: {
+                                       type: Array,
+                                       required: [true, "Buyer address required"]
+                                   },
+                                   phone: {
+                                       type: Number,
+                                       required: [true, "Buyer phone number required"],
+                                       minlength:[10,'Minimum phone length is 10 characters'],
+                                       maxlength:[10,'Minimum phone length is 10 characters']
+                                   },
+                                   gender: {
+                                       type: String,
+                                       required: [true, "Buyer gender required"],
+                                       enum: { values: ['Female', 'Male', 'Other'],
+                                           message: '{VALUE} gender is not supported'}
+                                   },
+                                   dob: {type: Number, required: [true, "Buyer dob required"]},
                                }, {collection: 'buyer'});
 export default schema;
